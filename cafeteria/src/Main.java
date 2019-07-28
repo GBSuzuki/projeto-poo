fcbc04ff99ae3a6ab11555e8477523934cecc0c9
Raphael Freitas <raphaeldefreitas2009@gmail.com>
2019-07-28 10:09:25 -0300

    Tentativa de implementação da GUI

--- cafeteria/src/Main.java
+++ cafeteria/src/Main.java
@@ -1,7 +1,15 @@
 import domain.Compra;
+import domain.MateriaPrima;
 import domain.Receita;
 import domain.Venda;
 import eu.lestard.easydi.EasyDI;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 import service.BancoDeReceitas;
 import service.Estoque;
 import service.PersistenceService;
@@ -11,10 +19,78 @@ import service.interfaces.IEstoque;
 import service.interfaces.IPersistenceService;
 import service.interfaces.ITranscaoService;
 
+import java.io.IOException;
 import java.sql.Timestamp;
 
-public class Main {
+public class Main extends Application {
+
+    private Stage primaryStage;
+    private BorderPane rootLayout;
+    private TabPane rootTabPane;
+
+    @Override
+    public void start(Stage primaryStage) {
+        this.primaryStage = primaryStage;
+        this.primaryStage.setTitle("Cafeteria");
+
+        initRootLayout();
+
+        addEstoqueTab();
+    }
+
+    /**
+     * Inicializa o root layout (layout base).
+     */
+    public void initRootLayout() {
+        try {
+            // Carrega o root layout do arquivo fxml.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(Main.class.getResource("view/RootLayout.fxml"));
+            rootLayout = (BorderPane) loader.load();
+            rootTabPane = (TabPane) rootLayout.getCenter();
+
+            // Mostra a scene (cena) contendo o root layout.
+            Scene scene = new Scene(rootLayout);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Mostra o person overview dentro do root layout.
+     */
+    public void addEstoqueTab() {
+        try {
+
+            // Carrega o FXML do estoque.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(Main.class.getResource("view/Estoque.fxml"));
+            BorderPane receitasOverview = (BorderPane) loader.load();
+
+            //Cria a tab 1 e define o conteudo do fxml.
+            Tab tabEstoque = new Tab();
+            tabEstoque.setText("Estoque");
+            tabEstoque.setContent(receitasOverview);
+            rootTabPane.getTabs().add(tabEstoque);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Retorna o palco principal.
+     *
+     * @return
+     */
+    public Stage getPrimaryStage() {
+        return primaryStage;
+    }
+
     public static void main(String[] args) {
+
         EasyDI easyDI = new EasyDI();
         easyDI.bindInterface(IEstoque.class, Estoque.class);
         easyDI.bindInterface(IBancoDeReceitas.class, BancoDeReceitas.class);
@@ -39,5 +115,7 @@ public class Main {
 
         var co = t.getCompras(new Timestamp(System.currentTimeMillis()));
         var ce = t.getVendas(new Timestamp(System.currentTimeMillis()));
+
+        launch(args);
     }
 }
---
+++ cafeteria/src/view/EstoqueController.java
@@ -0,0 +1,55 @@
+package view;
+
+import domain.MateriaPrima;
+import eu.lestard.easydi.EasyDI;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import service.Estoque;
+import service.interfaces.IEstoque;
+import service.interfaces.IPersistenceService;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.UUID;
+
+public class EstoqueController implements Initializable {
+    @FXML
+    private TableView<MateriaPrima> tbData;
+
+    @FXML
+    public TableColumn<MateriaPrima, UUID> uuid;
+
+    @FXML
+    public TableColumn<MateriaPrima, String> nome;
+
+    @FXML
+    public TableColumn<MateriaPrima, Integer> preco;
+
+    @FXML
+    TableColumn<MateriaPrima, Integer> qtd;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+        uuid.setCellValueFactory(new PropertyValueFactory<>("UUID"));
+        nome.setCellValueFactory(new PropertyValueFactory<>("Nome"));
+        preco.setCellValueFactory(new PropertyValueFactory<>("Preco"));
+        qtd.setCellValueFactory(new PropertyValueFactory<>("Quantidade"));
+
+        EasyDI easyDI = new EasyDI();
+        easyDI.bindInterface(IEstoque.class, Estoque.class);
+        Estoque e = easyDI.getInstance(Estoque.class);
+
+        ObservableList<MateriaPrima> Estoque = FXCollections.observableArrayList(
+                // Inserir cada materia prima aqui.
+        );
+
+        //add your data to the table here.
+        tbData.setItems(Estoque);
+    }
+}
